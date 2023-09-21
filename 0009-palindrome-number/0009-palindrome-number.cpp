10c77aca092db7481ddd9a4ec1347c27dc6bbb76
Devansh Rautela <91424721+devanshrautela@users.noreply.github.com>
2023-09-21 23:57:15 +0530

    Time: 7 ms (75.13%), Space: 6.3 MB (14.01%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string ans=to_string(x);
-        reverse(ans.begin(), ans.end());
-        long result=stol(ans);
-        if(x==result) return true;
-        else return false;
+        if(x<0) return false;
+        long long reversed=0;
+        long long temp=x;
+        while(temp!=0){
+            int digit=temp%10;
+            reversed= reversed*10+digit;
+            temp/=10;
+        }
+        return reversed==x;
     }
 };
