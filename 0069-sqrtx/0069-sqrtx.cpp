2180dc61f1f43a3aecb55a4bfbc21c6ad4e84fa5
Devansh Rautela <91424721+devanshrautela@users.noreply.github.com>
2023-10-02 23:00:17 +0530

    Time: 4 ms (37.02%), Space: 6.3 MB (19.18%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int ans=0;
-        if(x==0) return x;
-        for(long i=0; i<=x; i++){
-            if(i*i<=x) ans=i;
-            else break;
-        }
-        return ans;
+        return sqrt(x);
     }
 };
